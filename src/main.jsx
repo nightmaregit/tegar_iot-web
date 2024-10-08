c39c751a752f92b76904139af1f65d7546a07e77
nightmaregit <nightmaregit@gmail.com>
2024-10-08 21:43:06 +0700

    rubah

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
- import { StrictMode } from 'react'
- import App from './App.jsx'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import { StrictMode } from "react";
+import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <ChakraProvider>
+      <CSSReset />
       <App />
     </ChakraProvider>
   </StrictMode>
