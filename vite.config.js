56ea9219604db91090fe0a23a04b1b85ccde2d65
nightmaregit <nightmaregit@gmail.com>
2024-10-09 22:45:59 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: false,  // Nonaktifkan source map
-  }
-})
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // Menyediakan fallback untuk aplikasi SPA
+  },
+});
